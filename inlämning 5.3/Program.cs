6640187eda0fbda60d649701ac4d8bc12d9f8e4a
7R1XY <theo.broden@gmail.com>
2023-11-22 10:22:52 +0100

    Add project files.

---
+++ inlämning 5.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace inlämning_5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[3];
+            Console.WriteLine("Skriv in tre tal på sammma rad med mellanrum");
+            string tal = Console.ReadLine();
+            string[] y = tal.Split(' ');
+
+            int a = int.Parse(y[0]);
+            int b = int.Parse(y[1]);
+            int c = int.Parse(y[2]);
+
+            int sum = (a + b + c);
+
+            Console.WriteLine("Summan av dina tal är " + sum);
+            
+            Console.ReadKey();
+           
+        }
+    }
+}
